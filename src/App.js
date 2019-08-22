a0671b0be78b0b56fd6d0af3a974c3764fc230c0
krboelter <krboelter@gmail.com>
2019-08-21 19:34:08 -0600

    added some stretch stuff

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
+
+function CreateScoreBox(props) {
+  return (
+    <div className={props.team}>
+      <h2 className={props.team + "__name"}>{props.teamName}</h2>
+      <div className={props.team + "__score"}>{props.teamScore}</div>
+    </div>
+  )
+}
+
+
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0)
@@ -13,18 +24,11 @@ function App() {
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+          <CreateScoreBox team="home" teamScore={homeScore} teamName="Lions"/>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
           <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
+
+          <CreateScoreBox team="away" teamScore={awayScore} teamName="Tigers"/>
         </div>
         <BottomRow />
       </section>
